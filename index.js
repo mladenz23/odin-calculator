348916dbe91f4f57e9b193d20d5f65c66ded60bb
mladenz23 <mladen.zeljkovic@gmail.com>
2024-11-10 05:39:17 +0100

    Calculator project so far

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
-const buttons = document.querySelectorAll('.digit');
+const buttons = document.querySelectorAll('.onScreen');
+// const operators = document.querySelectorAll('.operator');
 const display = document.querySelector('#display');
+const equals = document.querySelector('.equals');
 
 let num1;
 let num2;
 let op;
 
-const add = function () {};
+const add = function (a, b) {
+  return a + b;
+};
 
-const subtract = function () {};
+const subtract = function (a, b) {
+  return a - b;
+};
 
-const multiply = function () {};
+const multiply = function (a, b) {
+  return a * b;
+};
 
-const divide = function () {};
-
-const operate = function (n1, n2, op) {};
+const divide = function (a, b) {
+  return a / b;
+};
 
 const displayNumber = function () {
   buttons.forEach(btn => {
     btn.addEventListener('click', function () {
-      let number = 0;
+      let tempNumber;
+      let index;
+      let ops = ['+', '-', '*', '/'];
+
       display.textContent += btn.textContent;
-      number = +display.textContent;
-      console.log(number);
+      tempNumber = display.textContent;
+
+      for (let i = 0; i < tempNumber.length; i++) {
+        if (ops.includes(tempNumber[i])) {
+          index = i;
+        }
+      }
+
+      num1 = +tempNumber.slice(0, index);
+      num2 = +tempNumber.slice(index + 1);
+      op = tempNumber[index];
+
+      add(num1, num2);
+
+      // console.log('Num1: ' + num1, 'Num2: ' + num2, 'Op: ' + op);
+      // console.log(tempNumber);
     });
   });
 };
 
-displayNumber();
+// fix
+const operate = function (n1, n2, op) {
+  displayNumber();
+  let result;
+
+  if (op === '+') result = add(n1, n2);
+  if (op === '-') result = subtract(n1, n2);
+  if (op === '*') result = multiply(n1, n2);
+  if (op === '/') result = divide(n1, n2);
+
+  equals.addEventListener('click', () => {
+    display.textContent = result;
+  });
+};
+
+operate(num1, num2, op);
