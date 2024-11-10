bd9243d560dc046a6ae7f9448f4be0765e252c41
mladenz23 <mladen.zeljkovic@gmail.com>
2024-11-10 03:16:24 +0100

    Create calculator basic structure

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a calculator project
+
+I'll be following the assignments based on the Odin Project.
---
+++ index.js
@@ -0,0 +1,9 @@
+let num1;
+let num2;
+let op;
+
+const add = function () {};
+const subtract = function () {};
+const multiply = function () {};
+const divide = function () {};
+const operate = function (n1, n2, op) {};
