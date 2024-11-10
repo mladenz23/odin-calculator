3d2b0349dc5e5791edb689ae25f671211be33702
mladenz23 <mladen.zeljkovic@gmail.com>
2024-11-10 04:11:37 +0100

    Display numbers on screen

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const buttons = document.querySelectorAll('.digit');
+const display = document.querySelector('#display');
+
 let num1;
 let num2;
 let op;
 
 const add = function () {};
+
 const subtract = function () {};
+
 const multiply = function () {};
+
 const divide = function () {};
+
 const operate = function (n1, n2, op) {};
+
+const displayNumber = function () {
+  buttons.forEach(btn => {
+    btn.addEventListener('click', function () {
+      let number = 0;
+      display.textContent += btn.textContent;
+      number = +display.textContent;
+      console.log(number);
+    });
+  });
+};
+
+displayNumber();
